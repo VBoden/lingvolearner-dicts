f34725e9db8e5bfdd0af8c66109ddbc7b413b34d
Володимир Боденчук <Bodenchuk@bigmir.net>
2022-08-23 21:58:59 +0300

    Added models.

--- words/admin.py
+++ words/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Dictionary, DictionaryEntry, Language, Word
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Dictionary)
+admin.site.register(DictionaryEntry)
+admin.site.register(Language)
+admin.site.register(Word)
+
+
+
+
--- words/models.py
+++ words/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Language(models.Model):
+    code = models.CharField(max_length=2, primary_key=True, default='na')
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Dictionary(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Word(models.Model):
+    word = models.CharField(max_length=30)
+    language = models.ForeignKey('Language', on_delete=models.CASCADE, null=False)
+
+    class Meta:
+        ordering = ['language', 'word']
+
+    def __str__(self):
+        return f'{self.word} ({str(self.language)})'
+
+
+class DictionaryEntry(models.Model):
+    word = models.ForeignKey('Word', on_delete=models.CASCADE, null=False)
+    transcription = models.CharField(max_length=30, blank=True, null=True)
+    translation = models.ForeignKey('Word', on_delete=models.CASCADE, related_name='translation')
+    language_from = models.ForeignKey('Language', on_delete=models.CASCADE, related_name='lang_from')
+    language_to = models.ForeignKey('Language', on_delete=models.CASCADE, related_name='to')
+    category = models.ManyToManyField(Category, blank=True)
+    dictionary = models.ManyToManyField(Dictionary, blank=True)
+
+    class Meta:
+        ordering = ['language_from', 'language_to', 'word', 'translation']
+
+    def get_transcription(self):
+        return "" if not self.transcription else self.transcription
+
+    def __str__(self):
+        return f'{self.language_from}-{self.language_to}: {self.word.word} [{self.get_transcription()}] - {self.translation.word}'
+
+    def get_absolute_url(self):
+        return reverse('dict-details', args=[str(self.id)])
