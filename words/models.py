f0bc25bcef7db0c65045980b42d78f912d1e5b00
Володимир Боденчук <Bodenchuk@bigmir.net>
2022-08-23 22:32:20 +0300

    Restructured models.

--- words/models.py
+++ words/models.py
@@ -19,14 +19,20 @@ class Category(models.Model):
 
 class Dictionary(models.Model):
     name = models.CharField(max_length=30)
+    language_from = models.ForeignKey('Language', on_delete=models.CASCADE, related_name='lang_from')
+    language_to = models.ForeignKey('Language', on_delete=models.CASCADE, related_name='to')
+
+    class Meta:
+        ordering = ['language_from', 'language_to', 'name']
 
     def __str__(self):
-        return self.name
+        return f'{self.name} ({self.language_from}-{self.language_to})'
 
 
 class Word(models.Model):
     word = models.CharField(max_length=30)
     language = models.ForeignKey('Language', on_delete=models.CASCADE, null=False)
+    category = models.ManyToManyField(Category, blank=True)
 
     class Meta:
         ordering = ['language', 'word']
@@ -39,19 +45,16 @@ class DictionaryEntry(models.Model):
     word = models.ForeignKey('Word', on_delete=models.CASCADE, null=False)
     transcription = models.CharField(max_length=30, blank=True, null=True)
     translation = models.ForeignKey('Word', on_delete=models.CASCADE, related_name='translation')
-    language_from = models.ForeignKey('Language', on_delete=models.CASCADE, related_name='lang_from')
-    language_to = models.ForeignKey('Language', on_delete=models.CASCADE, related_name='to')
-    category = models.ManyToManyField(Category, blank=True)
     dictionary = models.ManyToManyField(Dictionary, blank=True)
 
     class Meta:
-        ordering = ['language_from', 'language_to', 'word', 'translation']
+        ordering = ['word', 'translation']
 
     def get_transcription(self):
         return "" if not self.transcription else self.transcription
 
     def __str__(self):
-        return f'{self.language_from}-{self.language_to}: {self.word.word} [{self.get_transcription()}] - {self.translation.word}'
+        return f'{self.word.word} [{self.get_transcription()}] - {self.translation.word}'
 
     def get_absolute_url(self):
         return reverse('dict-details', args=[str(self.id)])
