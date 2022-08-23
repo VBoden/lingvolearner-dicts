853ceac05e1938c64587ae44db756b75936b1800
Володимир Боденчук <Bodenchuk@bigmir.net>
2022-08-23 21:58:23 +0300

    Begining changes.

--- lingvo_dicts/urls.py
+++ lingvo_dicts/urls.py
@@ -15,7 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += [
+    path('words/', include('words.urls')),
+]
+
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='words/', permanent=True)),
+]
+
+# Use static() to add URL mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
