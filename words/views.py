09459a0fc39c0b30b57cf142e8c528e4428ebe9c
Володимир Боденчук <Bodenchuk@bigmir.net>
2022-08-27 16:02:22 +0300

    Formatting.

--- words/views.py
+++ words/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.views import generic
 from django.urls import reverse
 from django.core.paginator import Paginator
@@ -25,6 +25,7 @@ class DictionaryEntriesView(generic.ListView):
         context['form'] = self.form
         return context
 
+
 def index(request):
     """View function for home page of site."""
 
@@ -49,6 +50,7 @@ def index(request):
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
+
 def categories_dicts(request):
 
     context = {
@@ -70,21 +72,21 @@ def filters(request):
                 request.session['dictionary'] = None if not fc['dictionary'] else fc['dictionary'].pk
                 request.session['dictionary_exclude'] = fc['dictionary_exclude']
         elif('_reset_filters' in request.POST):
-            param_names = ['category','category_exclude','dictionary','dictionary_exclude']
+            param_names = ['category', 'category_exclude', 'dictionary', 'dictionary_exclude']
             for param in param_names:
                 request.session[param] = None
     return redirect(reverse('allwords'), template_name='all_entries.html')
 
+
 def create_filter_form(request):
     initial = {}
-    print(request.session.get('category'))
     category_pk = request.session.get('category')
     if(category_pk):
-        initial['category']=Category.objects.get(pk=category_pk)
+        initial['category'] = Category.objects.get(pk=category_pk)
     dictionary_pk = request.session.get('dictionary')
     if(dictionary_pk):
-        initial['dictionary']=Dictionary.objects.get(pk=dictionary_pk)
-    simple_values = ['category_exclude','dictionary_exclude']
+        initial['dictionary'] = Dictionary.objects.get(pk=dictionary_pk)
+    simple_values = ['category_exclude', 'dictionary_exclude']
     for param in simple_values:
         value = request.session.get(param)
         if(value):
@@ -93,9 +95,10 @@ def create_filter_form(request):
     form = FiltersForm(initial=initial)
     return form
 
+
 def all_entries(request):
     page = 1
-    per_page = request.session.get('per_page',2)
+    per_page = request.session.get('per_page', 2)
     if(request.GET):
         page = request.GET.get("page", 1)
         per_page = request.GET.get("per_page", per_page)
@@ -117,9 +120,10 @@ def all_entries(request):
     }
     return render(request, 'all_entries.html', context=context)
 
+
 def category(request, pk):
     page = 1
-    per_page = request.session.get('per_page',2)
+    per_page = request.session.get('per_page', 2)
     if(request.GET):
         page = request.GET.get("page", 1)
         per_page = request.GET.get("per_page", per_page)
@@ -142,6 +146,7 @@ def category(request, pk):
     }
     return render(request, 'category.html', context=context)
 
+
 def manage_entries_request(request):
     
     if request.method == 'POST':
@@ -153,6 +158,7 @@ def manage_entries_request(request):
         print(request)
         form = UpdateEntriesForm()
     return form
+
 
 def dictionary(request, pk):
     form = manage_entries_request(request)
@@ -168,10 +174,11 @@ def dictionary(request, pk):
     }
     return render(request, 'dictionary.html', context=context)
 
+
 def add_entry(request, pk=None):
 #    return edit_entry(request,None)
 
-#def edit_entry(request, pk):
+# def edit_entry(request, pk):
     result = ''
     if request.method == 'POST':
         form = UpdateEntryForm(request.POST)
@@ -184,31 +191,31 @@ def add_entry(request, pk=None):
             if(fc['entry'] != None and not fc['entry_save_as_new']):
                 entry = fc['entry']
                 if(fc['word_save_as_new']):
-                    entry.word = create_word(fc['word'],from_lang,categories)
+                    entry.word = create_word(fc['word'], from_lang, categories)
                 else:
-                    change_word(entry.word,fc['word'],from_lang,categories)                                
+                    change_word(entry.word, fc['word'], from_lang, categories)                                
                 
                 if(fc['translation_save_as_new']):
-                    entry.translation = create_word(fc['translation'],to,categories)
+                    entry.translation = create_word(fc['translation'], to, categories)
                 else:
-                    change_word(entry.translation,fc['translation'],to,categories)
+                    change_word(entry.translation, fc['translation'], to, categories)
                 entry.transcription = fc['transcription']
                 entry.save()
                 entry.dictionary.set(fc['dictionaries'])
-                result = "Seccessfuly added: "+ str(entry)
+                result = "Seccessfuly added: " + str(entry)
             else:
-                word = get_or_create_word(fc['word'],from_lang,categories)
-                translation = get_or_create_word(fc['translation'],to,categories)
-                entry = DictionaryEntry(word = word,translation=translation)
+                word = get_or_create_word(fc['word'], from_lang, categories)
+                translation = get_or_create_word(fc['translation'], to, categories)
+                entry = DictionaryEntry(word=word, translation=translation)
                 
                 entry.transcription = fc['transcription']
                 entry.save()
                 entry.dictionary.set(fc['dictionaries'])
-                result = "Seccessfuly added: "+ str(entry) 
+                result = "Seccessfuly added: " + str(entry) 
         if('_save' in request.POST):
             return all_entries(request)        
         elif('_continue' in request.POST):
-            path=re.sub('/(\d)*$','/'+str(entry.pk),request.path_info)
+            path = re.sub('/(\d)*$', '/' + str(entry.pk), request.path_info)
             context = {
                 'form':form,
                 'result':result
@@ -216,7 +223,7 @@ def add_entry(request, pk=None):
             return redirect(path, template_name='edit_entry.html', context=context)            
         elif('_addanother' in request.POST):
             form = get_initial_update_form()
-            path=re.sub('/(\d)*$','/',request.path_info)
+            path = re.sub('/(\d)*$', '/', request.path_info)
             context = {
                 'form':form,
                 'result':result
@@ -237,23 +244,25 @@ def add_entry(request, pk=None):
     }
     return render(request, 'edit_entry.html', context=context)
 
+
 def get_initial_update_form(entry=None):
     from_lang = Language.objects.get(code='es')
     to_lang = Language.objects.get(code='uk')
     if(entry != None):
-        initial={'from_lang': entry.word.language.code,'to':entry.translation.language.code} 
-        initial['entry']=entry
-        initial['word']=entry.word.word
-        initial['transcription']=entry.transcription
-        initial['translation']=entry.translation.word
-        initial['categories']=entry.word.category.all().values_list("pk", flat=True)
-        initial['dictionaries']=entry.dictionary.all().values_list("pk", flat=True)     
+        initial = {'from_lang': entry.word.language.code, 'to':entry.translation.language.code} 
+        initial['entry'] = entry
+        initial['word'] = entry.word.word
+        initial['transcription'] = entry.transcription
+        initial['translation'] = entry.translation.word
+        initial['categories'] = entry.word.category.all().values_list("pk", flat=True)
+        initial['dictionaries'] = entry.dictionary.all().values_list("pk", flat=True)     
     else:
-        initial={'from_lang': from_lang.code,'to':to_lang}        
+        initial = {'from_lang': from_lang.code, 'to':to_lang}        
     form = UpdateEntryForm(initial)
     return form
 
-def get_or_create_word(word_text,lang,categories):
+
+def get_or_create_word(word_text, lang, categories):
     try:
         return Word.objects.filter(word=word_text).first()
     except ObjectDoesNotExist:
@@ -264,19 +273,22 @@ def get_or_create_word(word_text,lang,categories):
     word.save()
     return word
 
-def create_word(word_text,lang,categories):
+
+def create_word(word_text, lang, categories):
     word = Word(word=word_text)
     word.language = lang
     word.save()
     word.category.set(categories)
     word.save()
     return word
+
        
-def change_word(entry_word,word_text,lang,categories): 
+def change_word(entry_word, word_text, lang, categories): 
     entry_word.word = word_text
     entry_word.language = lang
     entry_word.category.set(categories)
     entry_word.save()
+
     
 def edit_entries(request):
     form = UpdateEntriesForm(request.POST)
